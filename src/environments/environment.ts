9be4c3f6b00f08aa84486c2d8c99c1283b9fd64e
Tlholo Makhene <99732888+TlholoMDeving@users.noreply.github.com>
2022-03-05 20:20:51 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebaseConfig: {
-    apiKey: 'AIzaSyBkZyluiSjDauektBYTKIc7_jXywBYW8QU',
-    authDomain: 'angular-material-crud-products.firebaseapp.com',
-    projectId: 'angular-material-crud-products',
-    storageBucket: 'angular-material-crud-products.appspot.com',
-    messagingSenderId: '274007146974',
-    appId: '1:274007146974:web:fe25456c9ca906290d1d4f',
+    apiKey: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+    authDomain:'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+    projectId: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+    storageBucket:'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+    messagingSenderId: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX',
+    appId: 'XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX'',
   },
 };
 
